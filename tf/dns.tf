44958e5f05ad1a402449b616daa1cedc430f96cf
charbonnierg <guillaume.charbonnier@araymond.com>
2022-06-25 14:23:22 +0200

    support domain and subdomains for DOMAIN_NAME variable

--- tf/dns.tf
+++ tf/dns.tf
@@ -6,7 +6,7 @@ data "digitalocean_domain" "lxx_domain" {
 resource "digitalocean_record" "lxx_domain" {
   domain = data.digitalocean_domain.lxx_domain.id
   type   = "A"
-  name   = trimsuffix(trimsuffix(var.domain, var.domain_name), ".")
+  name   = coalesce(trimsuffix(trimsuffix(var.domain, var.domain_name), "."), "@")
   value  = digitalocean_droplet.lxx-droplet.ipv4_address
 }
 
